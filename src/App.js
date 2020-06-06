a70f5cce66f1a7ff9a209c596ae8a5e2c907f839
fosgen <olegkkvv@gmail.com>
2020-06-06 14:14:17 +0300

    Добавлен роут Route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
@@ -10,6 +11,7 @@ const App = () => {
       <Header />
       <Navbar />
       <Profile />
+      <Route />
     </div>);
 }
 
