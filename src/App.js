9a801ed18f341f6731358115967dbf9cd446a7bf
Fosgen-working <klochkovod@yandex.ru>
2020-06-09 12:51:03 +0300

    Исправил App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
 
