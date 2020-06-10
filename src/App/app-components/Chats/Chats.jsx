d74ffaaf0618fe4cd0f74f23e89d059b07934a49
fosgen <olegkkvv@gmail.com>
2020-06-11 00:11:47 +0300

    Создание панели чата

--- src/App/app-components/Chats/Chats.jsx
+++ src/App/app-components/Chats/Chats.jsx
@@ -4,7 +4,10 @@ import './Chats.scss';
 const Chats = () => {
   return (
     <div className='Chats'>
-
+      <div className="sidebar-name">
+        <span>Чаты</span>
+        <button type="button" name="new_chat"></button>
+      </div>
     </div>
   );
 }
