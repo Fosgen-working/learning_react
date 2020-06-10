5a0e751415b13dbcd51ce077110920742e0fa9a8
fosgen <olegkkvv@gmail.com>
2020-06-10 11:01:34 +0300

    Добавление папок

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Chats/Chats';
 
 const App = () => {
   return (
