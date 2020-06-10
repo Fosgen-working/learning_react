2b0b56b7951021f9d35c290f6e38a542dd21f4ed
fosgen <olegkkvv@gmail.com>
2020-06-10 11:07:08 +0300

    Изменение папок

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Chats/Chats';
+import Navbar from './app-components/Navbar/Navbar';
+import Profile from './app-components/Chats/Chats';
 
 const App = () => {
   return (
